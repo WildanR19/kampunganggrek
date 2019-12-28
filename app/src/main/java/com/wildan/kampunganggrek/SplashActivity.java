3d0842667297a5233a02ca8e0aad2f4d786afd99
WildanR19 <hyakuya7856@gmail.com>
2019-12-28 22:41:07 +0700

    Initial commit

---
+++ app/src/main/java/com/wildan/kampunganggrek/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.wildan.kampunganggrek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private int waktu_loading=2000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home = new Intent(SplashActivity.this, WelcomeActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },waktu_loading);
+    }
+}
